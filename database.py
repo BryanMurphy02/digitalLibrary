efee918a5a0cb088442f72f30027aad1c2e1d6cf
Bryan Murphy <bryanmurphy02@gmail.com>
2025-08-18 20:27:31 -0400

    create database file for initial connection and querying

---
+++ database.py
@@ -0,0 +1,29 @@
+import psycopg2
+from psycopg2.extras import RealDictCursor #returns python dictionaries for a query instead of tuples
+
+#Connect to the database
+def get_connection():
+    return psycopg2.connect(
+        host = "localhost",
+        database = "Digital_Library",
+        user = "postgres",
+        password = "8623"
+    )
+
+#pass in a query statement
+def query_database(query, params=None, fetchone=False):
+    con = get_connection()
+    cur = con.cursor(cursor_factory=RealDictCursor)
+
+    cur.execute(query, params or ()) #uses an empty tuple if there are no params
+
+    data = None
+    if fetchone:
+        data = cur.fetchone()
+    else:
+        data = cur.fetchall()
+
+    con.commit()
+    cur.close()
+    con.close()
+    return data
