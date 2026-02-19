da1df88be1423aa08d5bc2ceda13b81c5229cb36
Bryan Murphy <bryanmurphy02@gmail.com>
2026-02-18 20:56:29 -0500

    moved database over to docker run database

--- database.py
+++ database.py
@@ -1,14 +1,28 @@
 import psycopg2
 from psycopg2.extras import RealDictCursor #returns python dictionaries for a query instead of tuples
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # reads the .env file and loads the variables into the environment
+
 
 #Connect to the database
 def get_connection():
     return psycopg2.connect(
-        host = "localhost",
-        database = "Digital_Library",
-        user = "postgres",
-        password = "8623"
+        host=os.environ.get("DB_HOST"),
+        port=os.environ.get("DB_PORT"),
+        database=os.environ.get("DB_NAME"),
+        user=os.environ.get("DB_USER"),
+        password=os.environ.get("DB_PASSWORD")
     )
+
+# def get_connection():
+#     return psycopg2.connect(
+#         host = "localhost",
+#         database = "Digital_Library",
+#         user = "postgres",
+#         password = "8623"
+#     )
 
 #pass in a query statement
 def query_database(query, params=None, fetchone=False):
